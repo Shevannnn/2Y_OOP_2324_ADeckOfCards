4efd3131e0d914098ff3fe292b4f215d0a3a34e8
Shevannnn <evaneldridge.palo@gmail.com>
2023-10-27 17:57:34 +0800

    Finished blackjack logic

--- 2Y_OOP_2324_ADeckOfCards/Program.cs
+++ 2Y_OOP_2324_ADeckOfCards/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _2Y_OOP_2324_ADeckOfCards
@@ -10,20 +12,8 @@ namespace _2Y_OOP_2324_ADeckOfCards
     {
         static void Main(string[] args)
         {
-            DeckOfCards doc = new DeckOfCards(true);
-            Card draw = doc.drawACard();
-            Console.WriteLine("Drawing a card");
-            Console.WriteLine($"The Card is the {draw.GetCardFace()} of {draw.GetCardSuit()} with a value of {draw.GetCardValue()}");
-            //doc.DisplayDeck();
-
-            Console.WriteLine("\n\nDrawing a couple of cards");
-            List<Card> hand = new List<Card>();
-            hand = doc.drawACard(5);
-            foreach (Card c in hand)
-            {
-                Console.WriteLine($"The Card is the {c.GetCardFace()} of {c.GetCardSuit()} with a value of {c.GetCardValue()}");
-            }
-            Console.ReadKey();
+            Blackjack blackjack = new Blackjack();
+            blackjack.Play();
         }
     }
 }
